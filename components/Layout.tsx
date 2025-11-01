dbf82f8d0bc47e97469c6a5192ea99bdbe166ba9
Sherif Ali <elemlak202099@gmail.com>
2025-11-02 01:02:59 +0200

    Fix case sensitivity issue in Navbar import path

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,5 +1,5 @@
 import { Footer } from '@/components/Footer';
-import { Navbar } from '@/components/Navbar';
+import { Navbar } from '@/components/navbar';
 
 interface LayoutProps {
   children: React.ReactNode;
